4d29cebc17b44902f6852995f4c465de9b36cc93
Jamie Brown <jmbrowncow@gmail.com>
2018-09-14 00:41:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
       result = "This one is on me!";
     } else if (someValue > 2000){
-      result = 30
+      result = "I will gladly take your thirty bucks."
     }
 
   return result;
