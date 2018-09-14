39efeb9d01707ddc6397abb102bed76411364d13
Jamie Brown <jmbrowncow@gmail.com>
2018-09-14 00:34:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,7 @@
 function scuberGreetingForFeet(someValue) {
   //this is where we can use conditionals given our argument, someValue
   //don't forget to return whatever the result is!
+  case someValue >= 400:
+    "This one is on me!";
+    break;
 }
